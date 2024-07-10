fcb31f3abe7e157977579d8cfe7c9a7bf3fdc029
pytuna <zikzikzikzik16@gmail.com>
2024-07-10 15:32:16 +0700

    update animation

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -1,9 +1,50 @@
-import { Box, Flex, Stack, Tag, Text, Image, Grid, GridItem, Container, useColorModeValue } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Stack,
+  Tag,
+  Text,
+  Image,
+  Grid,
+  GridItem,
+  Container,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import skills from '@/data/skills';
-import { motion } from 'framer-motion';
+import { motion,  useAnimation } from 'framer-motion';
+import { useRef, useEffect } from 'react';
 
-function SkillBox({ logo, name }) {
-  // animate scale the skill box when hovered
+function SkillBox({ logo, name, index }) {
+  var controls = useAnimation();
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          console.log("Intersecting");
+          controls.start({
+            y: [0, -10, 0], // Chuyển động theo trục y, nảy lên rồi trở về vị trí cũ
+            transition: {
+              duration: 0.5,
+              delay: index * 0.1, // Độ trễ tăng dần cho mỗi box
+            },
+          });
+        }
+      },
+      { threshold: 0.1 }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    var refCurrent = ref.current;
+    return () => {
+      if (refCurrent) {
+        observer.unobserve(refCurrent);
+      }
+    };
+  }, [controls, index]);
   const hoverVariants = {
     hover: {
       scale: 1.1,
@@ -13,9 +54,9 @@ function SkillBox({ logo, name }) {
     },
   };
   return (
-    <Box width={'6.25rem'} cursor={'pointer'}>
+    <Box width={'5.25rem'} cursor={'pointer'}>
       <Box px={4}>
-        <motion.div whileHover={'hover'} variants={hoverVariants}>
+        <motion.div  initial={{ y: 0 }} animate={controls} ref={ref} whileHover={'hover'} variants={hoverVariants}>
           <Image src={logo} alt={name} objectFit="cover" mb={2} />
         </motion.div>
       </Box>
@@ -28,7 +69,7 @@ function SkillBox({ logo, name }) {
 
 function SkillsSection() {
   return (
-    <Box bg={useColorModeValue('gray.50' ,'gray.900')}>
+    <Box bg={useColorModeValue('gray.50', 'gray.900')}>
       <Container
         centerContent
         maxW={{
@@ -38,7 +79,7 @@ function SkillsSection() {
         pt={12}
         pb={10}
       >
-        <Stack  pb={4}>
+        <Stack pb={4}>
           <Flex justifyContent={'center'}>
             <Tag px={6} borderRadius={'xl'}>
               Skills
@@ -60,7 +101,7 @@ function SkillsSection() {
         >
           {skills.map((skill, index) => (
             <GridItem key={index} display={'flex'} justifyContent={'center'}>
-              <SkillBox {...skill} />
+              <SkillBox {...skill} index={index} />
             </GridItem>
           ))}
         </Grid>
